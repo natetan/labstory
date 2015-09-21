d7a538240908096f5ac45397d7e0a75864ef3dce
yulongtan <swifties@uw.edu>
2015-09-20 22:29:27 -0700

    ran a test to check word count

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		Story story = new Story();
-		for (int i = 0; i <= 35; i++) {
+		for (int i = 0; i <= 36; i++) {
 			System.out.println("Page " + i + ": " + story.getWordCount(story.getPage(i).getText()) 
 					+ " words and " + story.getCharCount(story.getPage(i).getText()) + " characters");
 		}
